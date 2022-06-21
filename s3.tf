496d3dfacb6e3d5135815e5041b1ea04abefbdcd
srkadiyam <106543169+srkadiyam@users.noreply.github.com>
2022-06-21 19:52:43 +0100

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "subbu"
+
+  grant {
+    id          = data.aws_canonical_user_id.current_user.id
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
+
+  grant {
+    type        = "Group"
+    permissions = ["READ_ACP", "WRITE"]
+    uri         = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+  }
+}
